f15de85aff05876ceb63fe88d0125bd7d92e17bc
boxp <tiyotiyouda@gmail.com>
2013-05-02 12:34:08 +0900

    cancelボタンで終了出来なかったバグを修正

--- src/net/boxp/meganecable/MainActivity.java
+++ src/net/boxp/meganecable/MainActivity.java
@@ -76,6 +76,7 @@ public class MainActivity extends Activity
               })
               .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                   public void onClick(DialogInterface dialog, int whichButton) {
+                        MainActivity.this.finish();
                   }
               })
               .show();
